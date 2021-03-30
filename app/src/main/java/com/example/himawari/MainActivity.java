1066730fc829a0e90358ec4d1daa9e9f2b3cc7d0
y4kw <y4kw@math.sci.hokudai.ac.jp>
2021-03-30 11:50:47 +0900

    landscape and portrait

--- app/src/main/java/com/example/himawari/MainActivity.java
+++ app/src/main/java/com/example/himawari/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.himawari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
@@ -10,7 +11,8 @@ import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String urlHimawari = "https://www.jma.go.jp/bosai/map.html#4/40.000/110.000&elem=vap&contents=himawari";
+    String urlHimawari = "https://www.jma.go.jp/bosai/map.html#3/40.000/120.000&elem=vap&contents=himawari";
+    String urlHimawariLandscape = "https://www.jma.go.jp/bosai/map.html#4/40.000/110.000&elem=vap&contents=himawari";
     private WebView webview;
 
     @Override
@@ -56,8 +58,17 @@ public class MainActivity extends AppCompatActivity {
         //webview.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
         //webview.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0");
         //webview.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.54");
+        int orientation = getResources().getConfiguration().orientation;
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            webview.loadUrl(urlHimawariLandscape);
+        }
+
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            webview.loadUrl(urlHimawari);
+        }
         //log();
-        webview.loadUrl(urlHimawari);
+        //webview.loadUrl(urlHimawari);
         //if (savedInstanceState == null) {
         //    log("savedInstanceState==null");
         //    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
